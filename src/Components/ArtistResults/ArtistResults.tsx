066e122c302b78e1decb661e6246a621ccc69d29
Brett Kuhn <brettkuhn87@gmail.com>
2023-01-03 09:52:59 -0700

    Added component and helper files and folders

---
+++ src/Components/ArtistResults/ArtistResults.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ArtistResults = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ArtistResults
---
+++ src/Components/UserCollection/UserCollection.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserCollection = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UserCollection
