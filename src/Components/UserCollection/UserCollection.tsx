3d2112c6f23d0681eb2e0fd9d0059615fda6e91f
Josh Pierce <joshua.alan.pierce@gmail.com>
2023-01-03 17:33:33 -0700

    Add sections and state

--- src/Components/UserCollection/UserCollection.tsx
+++ src/Components/UserCollection/UserCollection.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './_UserCollection.scss'
+
 
 const UserCollection = () => {
+  const[myCollection, setCollection] = useState('')
+  const[filterCollection, setFilter] = useState('')
+
   return (
-    <div>
+    <main className='my-collection'>
+      <div className='form'>
+        <input
+          type='text'
+          placeholder='Search by Album'
+          value={filterCollection}
+          onChange={event => setFilter(event.target.value)}
+        />
+      <button>Search</button>
+    </div>
+    <div className='album-display'>
       
     </div>
+    </main>
+    
+   
   )
 }
 
