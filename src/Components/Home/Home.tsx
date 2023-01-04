26dc8768aaf2c6c9517de751a3d39ba74f4b8a6e
Zac Walters <zacwalters4@gmail.com>
2023-01-04 02:04:03 -0700

    Add Link to my collection

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -6,14 +6,12 @@ const Home = () => {
   return (
     <div className="split-screen">
       <Link className="artist-search split" to={`/search`}>
-        <div >
           <h1>Search Artists</h1>
-        </div>
       </Link>
 
-      <div className="user-collection split">
-        <h1>My Collection</h1>
-      </div>
+      <Link className="user-collection split" to={`/my-collection`}>
+          <h1>My Collection</h1>
+      </Link>
     </div>
   )
 }
