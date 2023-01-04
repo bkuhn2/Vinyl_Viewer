8a92e6b3d32247bb3d7107217b4a2de0cf97c195
Josh Pierce <joshua.alan.pierce@gmail.com>
2023-01-04 12:22:15 -0700

    Refactor to clean up props

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -7,16 +7,23 @@ import SearchForm from '../SearchForm/SearchForm';
 import AlbumDetails from '../AlbumDetails/AlbumDetails';
 import UserCollection from '../UserCollection/UserCollection';
 
+interface SavedAlbum {
+  id: number
+  albumTitle: string,
+  artist: string, 
+  year: number,
+  coverUrl: string
+}
+
 
 function App() {
-  const[allAlbums, setAlbums] = useState([
-    {title: "", id: 0, image: ""}])
-      // {title: "tom petty", id: 1, image:"dummy url"}, 
-      // {title: "Master of Puppets", id: 2, image:"dummy url 2"}
-    // ]//)
-
-  const addToMyCollection = (newAlbum: {title: string, id: number, image: string}) => {
-    setAlbums(allAlbums => [...allAlbums, newAlbum])
+  const[userCollection, setUserCollection] = useState<SavedAlbum[]>([
+      {id: 1, albumTitle: "Ride the Lightning", artist: "Metallica", year: 1980, coverUrl: "image"}, 
+      {id: 2, albumTitle: "Master of Puppets", artist: "Metallica", year: 1975, coverUrl: "image"}
+    ])
+    
+  const addToCollection = (album: SavedAlbum): void => {
+    setUserCollection(allAlbums => [...allAlbums, album])
   }
   
   return (
@@ -28,7 +35,7 @@ function App() {
           <Route path=':artistName' element={<SearchForm />}/>
         </Route>
         <Route path='/album/:id' element={<AlbumDetails />}/>
-        <Route path='/my-collection' element={<UserCollection savedAlbums={allAlbums}/>}/>
+        <Route path='/my-collection' element={<UserCollection savedAlbums={userCollection}/>}/>
       </Route>
     </Routes>
   );
--- src/Components/UserCollection/UserCollection.tsx
+++ src/Components/UserCollection/UserCollection.tsx
@@ -1,41 +1,103 @@
-import React, { useState } from 'react'
+import React, { useState, FC } from 'react'
 import './_UserCollection.scss'
-import AlbumCard from '../AlbumCard/AlbumCard'
 
-interface AlbumsListProps {
-  savedAlbums: {title: string; id: number; image: string}[];
+interface AlbumsArray {
+  id: number
+  albumTitle: string,
+  artist: string, 
+  year: number,
+  coverUrl: string
 }
 
+interface Props {
+  savedAlbums: AlbumsArray[]
+}
 
-const UserCollection = (savedAlbums: AlbumsListProps) => {
+const UserCollection: FC<Props> = ({ savedAlbums }) => {
   const[filterCollection, setFilter] = useState('')
   
-  return (
-    <main className='my-collection'>
-      <div className='form'>
-        <input
-          type='text'
-          placeholder='Search by Album'
-          value={filterCollection}
-          onChange={event => setFilter(event.target.value)}
-        />
-      <button>Search</button>
-    </div>
-    <div className='album-display'>
-     {savedAlbums.savedAlbums.map((album) => {
-      return (
-        <div>
-          <p>{album.title}</p>
-          {/* <p>{album.id}</p> */}
-          <p>{album.image}</p>
-        </div>
-      )
-     })}
-    </div>
-    </main>
+  const filterAlbums = () => {
     
-   
-  )
+    let myAlbums = savedAlbums.filter(album => {
+      return album.albumTitle === filterCollection
+    })  
+    console.log(myAlbums);
+    
+    return (
+      <main className='my-collection'>
+        <div className='form'>
+          <input
+            type='text'
+            placeholder='Search by Album'
+            value={filterCollection}
+            onChange={event => setFilter(event.target.value)}
+          />
+        <button onClick={() => filterAlbums()}>Search</button>
+        </div>
+        <div className='album-display'>
+        {myAlbums.map((album) => {
+          return (
+            <div>
+              <p>{album.albumTitle}</p>
+              <p>{album.artist}</p>
+              <p>{album.year}</p>
+              <p>{album.coverUrl}</p>
+            </div>
+        )
+      })}
+        </div>
+      </main>
+    )
+  }
+    if(savedAlbums.length === 0){
+      return(
+        <main className='my-collection'>
+        <div className='form'>
+          <input
+            type='text'
+            placeholder='Search by Album'
+            value={filterCollection}
+            onChange={event => setFilter(event.target.value)}
+          />
+        <button>Search</button>
+      </div>
+      <div className='album-display'>
+        <div>
+          <p>Nothing to display, go search and save some albums!</p>
+        </div>
+      </div>
+      </main>      
+    )
+  } else {
+  
+    return (
+      <main className='my-collection'>
+        <div className='form'>
+          <input
+            type='text'
+            placeholder='Search by Album'
+            value={filterCollection}
+            onChange={event => setFilter(event.target.value)}
+          />
+        <button onClick={() => filterAlbums()}>Search</button>
+      </div>
+      <div className='album-display'>
+      {savedAlbums.map((album) => {
+        return (
+          <div className='album-card'>
+            <p>{album.albumTitle}</p>
+            <p>{album.artist}</p>
+            <p>{album.year}</p>
+            <img src={album.coverUrl} alt={'Album cover of ' + album.albumTitle}></img>
+          </div>
+        )
+      })}
+      </div>
+      </main>
+      
+      
+    )
+    }
 }
 
 export default UserCollection
