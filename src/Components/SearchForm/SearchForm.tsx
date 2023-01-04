dcb4b92b169626ee7ed19ffed4fa8f2af0bdbc70
Brett Kuhn <brettkuhn87@gmail.com>
2023-01-04 09:40:20 -0700

    Added remaining state

--- src/Components/SearchForm/SearchForm.tsx
+++ src/Components/SearchForm/SearchForm.tsx
@@ -3,12 +3,19 @@ import React, { useState } from 'react'
 const SearchForm = () => {
 
   const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [albumsByArtist, setAlbumsByArtist] = useState([]);
 
   return (
     <div>
       <h1>Explore</h1>
       <form>
-        <input type='text' placeholder='Search for your favorite artists' value={searchTerm}/>
+        <input 
+          type='text' 
+          placeholder='Search for your favorite artists' 
+          value={searchTerm} 
+          onChange={event => setSearchTerm(event.target.value)}
+          />
         <button>Search</button>
       </form>
     </div>
