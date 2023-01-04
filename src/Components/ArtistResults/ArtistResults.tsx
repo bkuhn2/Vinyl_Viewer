fd7874470f984316bbb94b214b2a7474c704b4f7
Brett Kuhn <brettkuhn87@gmail.com>
2023-01-04 13:39:00 -0700

    Updated ArtistResults component to iterate over props it receives and display results

--- src/Components/ArtistResults/ArtistResults.tsx
+++ src/Components/ArtistResults/ArtistResults.tsx
@@ -1,17 +1,25 @@
 import React from 'react'
-import '../ArtistResults/_ArtistResults.scss'
+import '../ArtistResults/_ArtistResults.scss';
+import SearchResult from '../SearchResult/SearchResult';
 
 type Props = {
   name: string;
-  results: Array<object>;
+  results: Array<string>;
 }
 
 const ArtistResults = ({name, results}: Props) => {
+
+  const artistsList = results.map(result => {
+    return (
+      <SearchResult name={result}/>
+    )
+  })
+
   return (
     <section className='artist-results-section'>
       <h2>Results for "{name}"</h2>
       <div>
-
+        {artistsList}
       </div>
     </section>
   )
