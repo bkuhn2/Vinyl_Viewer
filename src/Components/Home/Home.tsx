10eb51853d30ab265651cb19ec71ef8834d536ff
Zac Walters <zacwalters4@gmail.com>
2023-01-04 02:00:10 -0700

    Add initial format and styling to the home page

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import './_Home.scss'
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      <h1>HOME</h1>
+    <div className="split-screen">
+      <Link className="artist-search split" to={`/search`}>
+        <div >
+          <h1>Search Artists</h1>
+        </div>
+      </Link>
+
+      <div className="user-collection split">
+        <h1>My Collection</h1>
+      </div>
     </div>
   )
 }
