0f9ff50bc6706d5cf300fb5c50d3191841de9bc9
Sam Rice <samsrice@gmail.com>
2023-01-04 16:52:29 -0800

    add conditional render of component when not loading

--- src/Components/AlbumDetails/AlbumDetails.tsx
+++ src/Components/AlbumDetails/AlbumDetails.tsx
@@ -66,67 +66,79 @@ const AlbumDetails: FC<Props> = ({addToCollection, userCollection}) => {
     )
   })
 
-  const previouslySavedMessage = <div className="saved-message">
-    <p>this album is saved in your collection</p>
-  </div>
+  const previouslySavedMessage = (
+    <div className="saved-message">
+      <p>this album is saved in your collection</p>
+    </div>
+  )
 
-  const articleIndex =  album?.article.indexOf("<")
+  const articleIndex = album?.article.indexOf("<")
   const formattedArticle = album?.article.substring(0, articleIndex) + "..."
 
   return (
     <>
-      <span className="directory">
-        <span className="directory__artist" data-cy="directory-artist">
-          {album?.artist}
-          {/* <Link /> to SearchForm with current artist as query can go here later */}
-        </span>
-        <span className="directory__album" data-cy="directory-album">
-          {" "}
-          / {album?.name}
-        </span>
-      </span>
-      <section className="album-section">
-        <div className="album-details">
-          <h1 className="album-details__name" data-cy="album-name">
-            {album?.name}
-          </h1>
-          {isSaved ? (
-            previouslySavedMessage
-          ) : (
-            <button
-              className="add-button"
-              data-cy="add-button"
-              onClick={() => handleSubmit()}
-            >
-              add to my collection
-            </button>
-          )}
-          <p className="album-details__date" data-cy="album-date">
-            released on: {releaseDate}
-          </p>
-          <article className="album-details__article" data-cy="album-article">
-            {formattedArticle}
-          </article>
-          <p className="album-details__last-link">
-            {"view more on "}
-            <a href={album?.lastURL} data-cy="album-link">
-              Last.fm
-            </a>
-          </p>
-          <ol className="album-details__tracklist" data-cy="album-tracklist">
-            {tracks}
-          </ol>
-        </div>
-        <div className="cover">
-          <div className="cover__mat">
-            <img
-              className="cover__mat__img"
-              src={ghTemp}
-              data-cy="album-cover"
-            />
-          </div>
-        </div>
-      </section>
+      {!isLoading && (
+        <>
+          <span className="directory">
+            <span className="directory__artist" data-cy="directory-artist">
+              {album?.artist}
+              {/* <Link /> to SearchForm with current artist as query can go here later */}
+            </span>
+            <span className="directory__album" data-cy="directory-album">
+              {" "}
+              / {album?.name}
+            </span>
+          </span>
+          <section className="album-section">
+            <div className="album-details">
+              <h1 className="album-details__name" data-cy="album-name">
+                {album?.name}
+              </h1>
+              {isSaved ? (
+                previouslySavedMessage
+              ) : (
+                <button
+                  className="add-button"
+                  data-cy="add-button"
+                  onClick={() => handleSubmit()}
+                >
+                  add to my collection
+                </button>
+              )}
+              <p className="album-details__date" data-cy="album-date">
+                released on: {releaseDate}
+              </p>
+              <article
+                className="album-details__article"
+                data-cy="album-article"
+              >
+                {formattedArticle}
+              </article>
+              <p className="album-details__last-link">
+                {"view more on "}
+                <a href={album?.lastURL} data-cy="album-link">
+                  Last.fm
+                </a>
+              </p>
+              <ol
+                className="album-details__tracklist"
+                data-cy="album-tracklist"
+              >
+                {tracks}
+              </ol>
+            </div>
+            <div className="cover">
+              <div className="cover__mat">
+                <img
+                  className="cover__mat__img"
+                  src={album?.image}
+                  data-cy="album-cover"
+                />
+              </div>
+            </div>
+          </section>
+        </>
+      )}
     </>
   )
 }
