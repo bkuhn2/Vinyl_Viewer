a6275b1d079738a0139bf4e7c9969c2b07510abf
Josh Pierce <joshua.alan.pierce@gmail.com>
2023-01-05 17:17:06 -0700

    Add test to simulate a user typing in the input

--- cypress/e2e/userCollection.cy.ts
+++ cypress/e2e/userCollection.cy.ts
@@ -11,8 +11,12 @@ describe("My Collection test", () => {
   it("Should have an input field and two buttons in the form", () => {
     cy.get(".form").within(() => {
       cy.get(".search-input")
-      cy.get(".filter-button")
+      cy.get(".filter-button").eq(0).contains("Search Artist")
+      cy.get(".filter-button").eq(1).contains("Clear Search Filter")
     }) 
+  })
+  it("Should display what the user has typed in the input field", () => {
+    cy.get(".search-input").type("heart").should("have.value", "heart")
   })
   it("Should display an image with an alt tag", () => {
     cy.get(".single-card").eq(0).within(() => {
