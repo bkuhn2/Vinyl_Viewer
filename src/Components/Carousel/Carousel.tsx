76e843ca2c18070cd42853c441bf5686810fbb9e
Zac Walters <zacwalters4@gmail.com>
2023-01-06 09:42:14 -0700

    Add basic carousel styling

--- src/Components/Carousel/Carousel.tsx
+++ src/Components/Carousel/Carousel.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import './_Carousel.scss';
 
 type Props = {
-  albums: Array<
-  {name: string, 
+  albums: Array<{
+    name: string, 
     picURL: string
-  }
-  >
+  }>
 }
 
 const Carousel = ({ albums }: Props) => {
@@ -13,14 +13,16 @@ const Carousel = ({ albums }: Props) => {
   const displayAlbums = albums.map((album, index) => {
     return (
       <div 
-        className="albumTile" 
+        className="album-tile" 
         key={index} 
-        style={{ 
-          backgroundImage: `url(${album.picURL})` 
-        }}
-      >{album.name}</div>
+      >
+        <img
+        src={album.picURL}
+        />
+        <h2>{album.name}</h2>
+      </div>
     ) 
-      })
+  })
 
     return (
       <div className="carousel-container">
