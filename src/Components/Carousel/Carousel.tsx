898e7715befa277f6d8ed11a50bb21a903ace909
Zac Walters <zacwalters4@gmail.com>
2023-01-06 09:22:28 -0700

    Add basic carousel functionality

--- src/Components/Carousel/Carousel.tsx
+++ src/Components/Carousel/Carousel.tsx
@@ -1,11 +1,34 @@
 import React from 'react'
 
-const Carousel = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+type Props = {
+  albums: Array<
+  {name: string, 
+    picURL: string
+  }
+  >
 }
+
+const Carousel = ({ albums }: Props) => {
+
+  const displayAlbums = albums.map((album, index) => {
+    return (
+      <div 
+        className="albumTile" 
+        key={index} 
+        style={{ 
+          backgroundImage: `url(${album.picURL})` 
+        }}
+      >{album.name}</div>
+    ) 
+      })
+
+    return (
+      <div className="carousel-container">
+        {displayAlbums}
+      </div>
+    )
+  }
+
+
 
 export default Carousel
