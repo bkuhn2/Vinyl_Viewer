d578f51e743a8500c63db74a2e9a7d4773a2d980
Zac Walters <zacwalters4@gmail.com>
2023-01-07 23:27:18 -0700

    Add home page navigation tests

--- cypress/e2e/Home_spec.cy.ts
+++ cypress/e2e/Home_spec.cy.ts
@@ -13,8 +13,27 @@ describe('Home Page', () => {
       .find('.artist-search')
         .should('have.css', 'background-image', 'url("http://localhost:3000/static/media/RecordsTop.9366c5709e1536e8f268.JPG")')
       .contains('Search Artists')
-
   })
 
+  it('should display my collection title and image', () => {
+    cy.get('.split-screen')
+      .find('.user-collection')
+        .should('have.css', 'background-image', 'url("http://localhost:3000/static/media/RecordsSide.aee915d23f40eedaf87b.JPG")')
+      .contains('My Collection')
+  })
+  
+  it('should navigate to the search page when search artists is clicked', () => {
+    cy.get('.split-screen')
+      .find('.artist-search')
+        .click()
+          .url().should('include', '/search')
+  })
+
+  it('should navigate to my collection when it is clicked', () => {
+    cy.get('.split-screen')
+      .find('.user-collection')
+        .click()
+          .url().should('include', '/my-collection')
+  })
   
 })
