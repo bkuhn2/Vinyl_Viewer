215bc494289edfa97243724f4ab3e35a79532254
Brett Kuhn <brettkuhn87@gmail.com>
2023-01-07 21:44:06 -0700

    Created component for the search error message element, same styling and JSX that was already repetitively typed

---
+++ src/Components/SearchError/SearchError.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../SearchError/_SearchError.scss'
+
+type Props = {
+  errorMessage: string;
+}
+
+const SearchError = ({ errorMessage } : Props) => {
+  return (
+    <div className='search-error-area'>
+      <h3 className='search-error-message'>{errorMessage}</h3>
+    </div>
+  )
+}
+
+export default SearchError
