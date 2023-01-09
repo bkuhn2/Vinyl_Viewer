2074e4403c348fdd2bffae7c0d4f48a04d8f805a
Josh Pierce <joshua.alan.pierce@gmail.com>
2023-01-09 11:57:00 -0700

    Add intercept for an album to save

--- cypress/e2e/userCollection.cy.ts
+++ cypress/e2e/userCollection.cy.ts
@@ -2,8 +2,19 @@
 
 describe("My Collection test", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000/my-collection")
-
+    // cy.visit("http://localhost:3000/my-collection")
+    cy.intercept(
+      {
+        method: "GET",
+        url: "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=fcf48a134034bb684aa87d0e0309a0fd&artist=the+beatles&album=rubber+soul&format=json",
+      },
+      {
+        fixture: "albumDetailsRubberSoul.json",
+      }
+    )
+    cy.visit("http://localhost:3000/album/the+beatles/rubber+soul")
+    cy.get(".add-button").click()
+    cy.get(":nth-child(2) > a > .nav-button").click()
   })
   it("Should display the header", () => {
     cy.contains("h1", "My Collection")
@@ -11,26 +22,16 @@ describe("My Collection test", () => {
   it("Should display a form", () => {
     cy.get(".form")
   })
-  it("Should have an input field and two buttons in the form", () => {
+  it("Should have an input field", () => {
     cy.get(".form").within(() => {
       cy.get(".search-input")
-      cy.get(".filter-button").contains("Search")
-      cy.get(".clear-filter-button").contains("Clear Search Filter")
     }) 
   })
-  it("Should display what the user has typed in the input field", () => {
-    cy.get(".search-input").type("metallica").should("have.value", "metallica")
+  it("Should have one album saved", () => {
+    cy.get(".album-image").should("have.length", "1")
   })
-  it("Should be able to save an album and display and filter the search", () => {
-    cy.visit("http://localhost:3000/album/metallica/master+of+puppets")
-      cy.get(".add-button").click()
-      cy.get(":nth-child(2) > a > .nav-button").click()
-      cy.get(".carousel-container").should("have.length", "1")
-      cy.get(".album-image").should("be.visible")
-      cy.get(".search-input").type("metallica").should("have.value", "metallica")
-      cy.get(".carousel-container").should("have.length", "1")
-      cy.get(".carousel-container").contains("Master of Puppets")
-      cy.get(".album-image").should("have.attr", "alt", "Album cover image of Master of Puppets")
-      cy.get(".clear-filter-button").click()
+  it("Should be able to delete an album", () => {
+    cy.get(".delete-album").click()
+    cy.get(".album-image").should("have.length", "0")
   })
 })
