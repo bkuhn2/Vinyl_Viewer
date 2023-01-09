74bccf091514a30a0d079e52ff5738d77d6ba6d2
Brett Kuhn <brettkuhn87@gmail.com>
2023-01-08 21:40:32 -0700

    Wrote tests for clicking on an artist and getting their albums, the error handling portion

--- cypress/e2e/search_errors.cy.ts
+++ cypress/e2e/search_errors.cy.ts
@@ -1,4 +1,4 @@
-import { verify } from "crypto";
+
 
 describe('Artist Search Error Handing', () => {
 
@@ -47,5 +47,79 @@ describe('Artist Search Error Handing', () => {
     cy.get('.search-error-message')
       .contains(`Unable to read the name you typed, please enter a validly formatted name.`)
   })
+
+  it('User should not be able to type certain characters', () => {
+    cy.get('.search-input').type(`&/#%?<>{}`)
+    cy.get('.search-input').should('have.value', '')
+  })
 })
 
+describe(`Artist's Album Search Error Handling`, () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/search')
+  })
+
+  it(`Should let the user know if there is an internal code error regarding fetch request when getting that artist's top albums`, () => {
+    cy.intercept(`http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=Smash%20Mouth&api_key=fcf48a134034bb684aa87d0e0309a0fd&format=json`, {
+      statusCode: 400
+    })
+    cy.intercept(`http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=Smash%20Mouth&api_key=fcf48a134034bb684aa87d0e0309a0fd%20%20%20%20&format=json`, {
+      method: 'GET',
+      fixture: '../fixtures/searchResults.json'
+    })
+    cy.get('.search-input').type('Smash Mouth')
+    cy.get('.search-button').click()
+    cy.get('.artist-results-section')
+      .get('#0').click()
+    cy.contains('Uh oh, looks like something went wrong in the back, please try again later.')
+  });
+
+  it('Should also let the user know if there is an issue with the backend API in general when trying to click on an artist name', () => {
+    cy.intercept(`http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=Smash%20Mouth&api_key=fcf48a134034bb684aa87d0e0309a0fd&format=json`, {
+      statusCode: 500
+    })
+    cy.intercept(`http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=Smash%20Mouth&api_key=fcf48a134034bb684aa87d0e0309a0fd%20%20%20%20&format=json`, {
+      method: 'GET',
+      fixture: '../fixtures/searchResults.json'
+    })
+    cy.get('.search-input').type('Smash Mouth')
+    cy.get('.search-button').click()
+    cy.get('.artist-results-section')
+      .get('#0').click()
+    cy.contains('Uh oh, looks like something went wrong in the back, please try again later.')
+  })
+
+  it('Should let the user know if the API sends back an ok reponse, but has an error message', () => {
+    cy.intercept(`http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=Bad%20Artist&api_key=fcf48a134034bb684aa87d0e0309a0fd%20%20%20%20&format=json`, {
+      method: 'GET',
+      fixture: '../fixtures/badArtist.json'
+    })
+    cy.intercept(`http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=Bad%20Artist&api_key=fcf48a134034bb684aa87d0e0309a0fd&format=json`, {
+      method: 'GET',
+      fixture: '../fixtures/badAlbum1.json'
+    })
+
+    cy.get('.search-input').type('Bad Artist')
+    cy.get('.search-button').click()
+    cy.get('.artist-results-section')
+      .get('#0').click()
+    cy.contains(`Couldn't find any albums for this artist`)
+  })
+
+  it('Should let the user know the data for the artist they clicked on does not have any albums in it', () => {
+    cy.intercept(`http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=Bad%20Artist&api_key=fcf48a134034bb684aa87d0e0309a0fd%20%20%20%20&format=json`, {
+      method: 'GET',
+      fixture: '../fixtures/badArtist.json'
+    })
+    cy.intercept(`http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=Bad%20Artist%20Collab&api_key=fcf48a134034bb684aa87d0e0309a0fd&format=json`, {
+      method: 'GET',
+      fixture: '../fixtures/badAlbum2.json'
+    })
+
+    cy.get('.search-input').type('Bad Artist')
+    cy.get('.search-button').click()
+    cy.get('.artist-results-section')
+      .get('#1').click()
+    cy.contains(`Couldn't find any albums for this artist`)
+  })
+})
