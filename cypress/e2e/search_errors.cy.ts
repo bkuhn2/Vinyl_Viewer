811aaa13c8b1eefc819fd1dc7d3cd3830e34b827
Brett Kuhn <brettkuhn87@gmail.com>
2023-01-08 19:41:16 -0700

    Finished writing tests for artist search error handling

--- cypress/e2e/search_errors.cy.ts
+++ cypress/e2e/search_errors.cy.ts
@@ -1,6 +1,6 @@
 import { verify } from "crypto";
 
-describe('Search Page Error Handing', () => {
+describe('Artist Search Error Handing', () => {
 
   beforeEach(() => {
     cy.visit('http://localhost:3000/search')
@@ -26,6 +26,26 @@ describe('Search Page Error Handing', () => {
       .contains('Uh oh, looks like something went wrong in the back, please try again later.')
   })
 
-  
+  it('Should let the user know if there are not any artists that meet their search term', () => {
+    cy.intercept(`http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=Smash%20Mouth&api_key=fcf48a134034bb684aa87d0e0309a0fd%20%20%20%20&format=json`, {
+      method: 'GET',
+      fixture: '../fixtures/noArtistMatches.json'
+    });
+    cy.get('.search-input').type('Smash Mouth')
+    cy.get('.search-button').click()
+    cy.get('.search-error-message')
+      .contains(`Looks like we don't have any artists matching that name...`)
+  })
 
+  it('Should let the user know if a band they type cannot be processed by the API', () => {
+    cy.intercept(`http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=Bad%20Name&api_key=fcf48a134034bb684aa87d0e0309a0fd%20%20%20%20&format=json`, {
+      method: 'GET',
+      fixture: '../fixtures/emptyArtistObject.json'
+    });
+    cy.get('.search-input').type('Bad Name')
+    cy.get('.search-button').click()
+    cy.get('.search-error-message')
+      .contains(`Unable to read the name you typed, please enter a validly formatted name.`)
+  })
 })
+
